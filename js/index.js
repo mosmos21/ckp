b3bfbbdc3f4622d7cf9c49abd42bb5b932f53e94
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2019-01-14 10:18:21 +0900

    またリファクタリング

--- js/index.js
+++ js/index.js
@@ -10,24 +10,19 @@ const ROTATE = [
 $(function () {
   const ctx = $('#cv')[0].getContext('2d')
   ctx.fillStyle = 'white'
-
   $('#download').click(() => {
     const url = window.URL.createObjectURL(createBlob(atob($('#cv')[0].toDataURL().split(',')[1])))
-    $(`<a id="link" href="${url}" download="export.png" />`)[0].click()
+    $(`<a href="${url}" download="export.png" />`)[0].click()
   })
-  $('#text').keyup(e => draw(ctx, $(e.target).val().split('\n')))
-  draw(ctx)
-})
-
-const draw = (ctx, textList = []) => {
-  var img = new Image()
-  img.src = 'image/back.png'
-  $(img).on('load', () => {
-    ctx.drawImage(img, 0, 0)
-    const fontSize = calcFontSize(textList)
-    ctx.font = `${fontSize}px メイリオ`
-    applyCharPosition(textList, fontSize)
-      .forEach(ele => {
+  $('#text').keyup(e => {
+    var img = new Image()
+    img.src = 'image/back.png'
+    $(img).on('load', () => {
+      ctx.drawImage(img, 0, 0)
+      const textList = $(e.target).val().split('\n') || []
+      const fontSize = calcFontSize(textList)
+      ctx.font = `${fontSize}px メイリオ`
+      applyCharPosition(textList, fontSize).forEach(ele => {
         const [offsetX, offsetY, rotate] = ROTATE.includes(ele.text)
           ? [0, fontSize * 0.1, Math.PI / 2]
           : [0, fontSize, 0]
@@ -37,8 +32,9 @@ const draw = (ctx, textList = []) => {
         ctx.fillText(ele.text, 0, 0)
         ctx.restore()
       })
-  })
-}
+    })
+  }).keyup()
+})
 
 const calcFontSize = textList => Math.min(
   Math.floor(WIDTH / (textList.length * 2 - 1)),
@@ -47,15 +43,15 @@ const calcFontSize = textList => Math.min(
 
 const applyCharPosition = (textList, fontSize) => {
   const lineWidth = WIDTH / (textList.length * 2 - 1)
-  return textList.reverse().map((str, row) =>
-    [...str].map((ch, col) => ({
+  return textList.reverse().map((str, row) => [...str].map(
+    (ch, col) => ({
       text: ch,
       x: LEFT + (row * (lineWidth * 2)) + (lineWidth - fontSize) / 2,
       y: TOP + (col * fontSize),
     }))).flat()
 }
 
-const createBlob = bin =>
-  new Blob(
-    [new Uint8Array(bin.length).map((_, i) => bin.charCodeAt(i)).buffer],
-    { type: 'image/png' })
+const createBlob = bin => new Blob(
+  [new Uint8Array(bin.length).map((_, i) => bin.charCodeAt(i)).buffer],
+  { type: 'image/png' }
+)
